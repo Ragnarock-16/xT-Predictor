207df51de594929d21d2afa34293b594571dfa1f
Ragnarock-16 <noureddinebert@gmail.com>
2024-05-27 14:54:34 +0200

    add baseline model

--- baseline.py
+++ baseline.py
@@ -10,8 +10,8 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 
 #increas batch
 BATCH_SIZE = 32
-#107 for GAME2, 250 G1,70 Full
-NUM_EPOCHS = 250
+#G1 = 10 ,G2 = 16
+NUM_EPOCHS = 21
 #decrease 
 LEARNING_RATE = 0.0001
 
@@ -29,17 +29,12 @@ class RLM(Dataset):
         
         with open(prefix + 'label.json', 'r') as file:
             self.image_label = torch.tensor(json.load(file))
-        with open(prefix + 'home_1.json', 'r') as file:
-            self.home_1 = torch.tensor(json.load(file))
-        with open(prefix + 'away_1.json', 'r') as file:
-            self.away_1 = torch.tensor(json.load(file))
-        with open(prefix + 'home_2.json', 'r') as file:
-            self.home_2 = torch.tensor(json.load(file))
-        with open(prefix + 'away_2.json', 'r') as file:
-            self.away_2 = torch.tensor(json.load(file))
+        with open(prefix + 'baseline.json', 'r') as file:
+            data = torch.tensor(json.load(file),dtype=torch.float32)
+            self.base = data.permute(0, 1, 4, 2, 3)
 
     def __getitem__(self,index):
-        return self.away_1[index], self.away_2[index], self.home_1[index], self.home_2[index], self.image_label[index]
+        return self.base[index], self.image_label[index]
 
     def __len__(self):
         return len(self.image_label)
@@ -51,39 +46,48 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.layer = nn.Sequential(
                     #out_channels too big add conv between, circular padding mode
-                    nn.Conv2d(in_channels=4, out_channels=16, kernel_size=7, stride=3, padding=5),
+                    nn.Conv2d(in_channels=3, out_channels=16, kernel_size=7, stride=3, padding=5),
+                    #nn.BatchNorm2d(16),
                     nn.ReLU(),
                     nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
                     nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=3, padding=3),
+                    #nn.BatchNorm2d(32),
                     nn.ReLU(),
                     nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
                     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=2),
+                    #nn.BatchNorm2d(64),
                     )
         conv_output_size = self._get_conv_output_size()
         print(conv_output_size)
-        self.fc1 = nn.Linear(conv_output_size, 128)
-        self.fc2 = nn.Linear(128,2)
+        self.fc1 = nn.Linear(conv_output_size, 1024)  # Adjust the input size
+        self.fc2 = nn.Linear(1024, 128)
+        self.fc3 = nn.Linear(128, 2)
+
+
 
     def _get_conv_output_size(self):
         # Dummy forward pass to get the output size of the last convolutional layer
         with torch.no_grad():
-            dummy_input = torch.zeros(1,4,15,120)
+            dummy_input = torch.zeros(1, 3, 450, 355)
             conv_output = self.layer(dummy_input)
-            conv_output_size = conv_output.view(conv_output.size(0), -1).size(1)
+            conv_output_size = (conv_output.view(conv_output.size(0), -1).size(1)) *2
 
         return conv_output_size
     
-    def forward(self, home_1, home_2, away_1, away_2):
-        # Stack input tensors along the channel dimension
-        x = torch.stack([home_1, home_2, away_1, away_2], dim=1)
-        x = self.layer(x)
+    def forward(self, base):
+
+        first_frame = self.layer(base[:,0])
+        last_frame = self.layer(base[:,1])
+        print(base.shape)
+        x = torch.cat((first_frame, last_frame), dim=1)
 
         # Flatten the output for fully connected layers
         x = x.reshape(x.size(0), -1)
 
         # Apply fully connected layers with activation functions, apply it to each layer
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
@@ -100,13 +104,13 @@ def run(train_loader, val_loader, test_loader, device):
     for epoch in range(NUM_EPOCHS):
         model.train()
         running_loss = 0.0  
-        for i, (away_1, away_2, home_1, home_2, labels) in enumerate(train_loader):
-            away_1, away_2, home_1, home_2, labels = away_1.to(device), away_2.to(device), home_1.to(device), home_2.to(device), labels.to(device)
+        for i, (base,labels) in enumerate(train_loader):
+            base, labels = base.to(device), labels.to(device)
 
             # Clear gradients w.r.t. parameters
             optimizer.zero_grad()
 
-            outputs = model(home_1, home_2, away_1, away_2)
+            outputs = model(base)
 
             # Compute the loss
             loss = criterion(outputs, labels)
@@ -128,9 +132,9 @@ def run(train_loader, val_loader, test_loader, device):
         total = 0
         
         with torch.no_grad():
-            for i, (away_1, away_2, home_1, home_2, labels) in enumerate(val_loader):
-                away_1, away_2, home_1, home_2, labels = away_1.to(device), away_2.to(device), home_1.to(device), home_2.to(device), labels.to(device)
-                outputs = model(home_1, home_2, away_1, away_2)
+            for i, (base,labels) in enumerate(val_loader):
+                base, labels = base.to(device), labels.to(device)
+                outputs = model(base)
                 loss = criterion(outputs, labels)
                 val_loss += loss.item()
                 
@@ -163,22 +167,22 @@ def run(train_loader, val_loader, test_loader, device):
     model.eval() 
     correct = 0
     total = 0
+
     predicted_labels = []
     true_labels = []
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
-        for i, (away_1, away_2, home_1, home_2, labels) in enumerate(test_loader):
-            away_1, away_2, home_1, home_2, labels = away_1.to(device), away_2.to(device), home_1.to(device), home_2.to(device), labels.to(device)
-            outputs = model(home_1, home_2, away_1, away_2)
+        for i, (base, labels) in enumerate(test_loader):
+            base, labels = base.to(device), labels.to(device)
+            outputs = model(base)
             # Calculate accuracy
             predicted = torch.argmax(outputs.data, dim=1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
             predicted_labels.extend(predicted.cpu().numpy())
             true_labels.extend(labels.cpu().numpy())
-                               
+
     print(f'Accuracy of the network on the test set: {100 * correct // total} %')
-    
     conf_matrix = confusion_matrix(true_labels, predicted_labels)
     print("Confusion Matrix:")
     print(conf_matrix)
@@ -190,7 +194,8 @@ def run(train_loader, val_loader, test_loader, device):
     print("Precision:", precision)
     print("Recall:", recall)
     print("F1 Score:", f1)
-    torch.save(model.state_dict(), 'model_Game1.pt')
+
+    #torch.save(model.state_dict(), 'V_model_game2.pt')
 
 def test_on_Game(game_type,model_name):
     dataset = RLM(game_type)
@@ -203,9 +208,9 @@ def test_on_Game(game_type,model_name):
     predicted_labels = []
     true_labels = []
     with torch.no_grad():
-        for i, (away_1, away_2, home_1, home_2, labels) in enumerate(test_loader):
-            away_1, away_2, home_1, home_2, labels = away_1.to(device), away_2.to(device), home_1.to(device), home_2.to(device), labels.to(device)
-            outputs = model(home_1, home_2, away_1, away_2)
+        for i, (base, labels) in enumerate(test_loader):
+            base, labels = base.to(device), labels.to(device)
+            outputs = model(base)
             # Calculate accuracy
             predicted = torch.argmax(outputs.data, dim=1)
             total += labels.size(0)
@@ -214,6 +219,7 @@ def test_on_Game(game_type,model_name):
             true_labels.extend(labels.cpu().numpy())
 
     print(f'Accuracy of the network on the test Game: {100 * correct // total} %')
+    
     conf_matrix = confusion_matrix(true_labels, predicted_labels)
     print("Confusion Matrix:")
     print(conf_matrix)
@@ -226,7 +232,6 @@ def test_on_Game(game_type,model_name):
     print("Recall:", recall)
     print("F1 Score:", f1)
 
-
 def compute_metrics(true_labels, predicted_labels):
     accuracy = accuracy_score(true_labels, predicted_labels)
     precision = precision_score(true_labels, predicted_labels)
@@ -234,7 +239,6 @@ def compute_metrics(true_labels, predicted_labels):
     f1 = f1_score(true_labels, predicted_labels)
     
     return accuracy, precision, recall, f1
-
 
 def plot_conf_matrix(conf_matrix):
     
@@ -255,12 +259,11 @@ def plot_conf_matrix(conf_matrix):
     plt.tight_layout()
     plt.show()
 
-
 def main():
-    dataset = RLM(game_type="game1")
+    dataset = RLM(game_type="full")
 
     train_size = int(0.8 * len(dataset))
-    val_size = int(0.1* len(dataset))
+    val_size = int(0.10 * len(dataset))
     test_size = len(dataset) - train_size - val_size
 
     # Use random_split to split the dataset
@@ -269,8 +272,9 @@ def main():
     train_loader = DataLoader(train_dataset,batch_size = BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(val_dataset,batch_size = BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test_dataset,batch_size = 1, shuffle=False)
+    print(len(dataset))
     run(train_loader,val_loader,test_loader,device)
-    test_on_Game(game_type="game2",model_name='model_Game1.pt')
+    #test_on_Game(game_type="game1",model_name='V_model_game2.pt')
 
 
     
